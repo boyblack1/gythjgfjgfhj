6ca5273d3a112992f72078d1c8222f473509c9a9
boyblack1 <kayron.bastos@to.estudante.senai.br>
2023-05-24 17:28:20 -0300

    configurando bootstrap e devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_23_183613) do
+ActiveRecord::Schema.define(version: 2023_05_24_180217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,9 +45,11 @@ ActiveRecord::Schema.define(version: 2023_05_23_183613) do
     t.string "state"
     t.text "summary"
     t.text "description"
-    t.string "publish"
+    t.boolean "publish"
+    t.bigint "company_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_positions_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,7 +64,7 @@ ActiveRecord::Schema.define(version: 2023_05_23_183613) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "applicants", "positions"
   add_foreign_key "applicants", "users"
   add_foreign_key "companies", "users"
+  add_foreign_key "positions", "companies"
 end
